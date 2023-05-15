a9f8ea8a2abf154e90ca9196a2333c488c9614d4
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-05-15 17:26:00 +0200

    Basic sudoku grid

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
 // import HelloWorld from './components/HelloWorld.vue'
 // import TheWelcome from './components/TheWelcome.vue'
+
+const gridSize = 9;
 </script>
 
 <template>
-  <main class="bg-black">
-    <p class="text-red-400">Hello World</p>
+  <main class="min-h-screen grid place-items-center">
+    <table class="border-collapse">
+      <tr v-for="row in gridSize" :key="row" class="bg-slate-200 text-center">
+        <td v-for="column in gridSize" :key="column"
+          :class="`p-2 border border-gray-300 w-12 h-12 ${(row % 3 === 0 && row !== gridSize) ? 'border-b-4' : ''} ${(column % 3 === 0 && column !== gridSize) ? 'border-r-4' : ''}`">
+          <span class="text-2xl"></span>
+        </td>
+      </tr>
+    </table>
   </main>
 </template>
