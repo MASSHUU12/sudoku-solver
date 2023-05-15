45263f14411a73ef7796b7fddd7ec993b425bc5c
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-05-15 17:33:43 +0200

    Grid store

---
+++ src/stores/grid.ts
@@ -0,0 +1,21 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useCounterStore = defineStore('grid', () => {
+  const grid = ref([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ])
+  function update(row: number, col: number, val: number): void {
+    grid.value[row][col] = val
+  }
+
+  return { grid, update }
+})
