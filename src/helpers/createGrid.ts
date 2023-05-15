fcf232bd9b991419dc75691b1c51e52c28f68555
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-05-15 21:41:45 +0200

    A simpler approach to creating a grid

--- src/helpers/createGrid.ts
+++ src/helpers/createGrid.ts
@@ -3,20 +3,10 @@
  * initialized with the given default value.
  *
  * @export
- * @param {number[]} [rows, cols] - An array containing the number of rows and columns.
+ * @param {[number, number]} [rows, cols] - An array containing the number of rows and columns.
  * @param {unknown} defaultValue - The default value to initialize the grid with.
  * @return {*}  {unknown[][]} The created grid with the specified dimensions and default value.
  */
-export function createGrid([rows, cols]: number[], defaultValue: unknown): unknown[][] {
-  let grid: unknown[][] = [];
-
-  for (let i = 0; i < rows; i++) {
-    grid[i] = [];
-
-    for (let j = 0; j < cols; j++) {
-      grid[i][j] = defaultValue;
-    }
-  }
-
-  return grid;
+export function createGrid([rows, cols]: [number, number], defaultValue: unknown): unknown[][] {
+  return Array.from({ length: rows }, () => Array(cols).fill(defaultValue));
 }
