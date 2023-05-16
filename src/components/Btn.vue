327ce5d88fa725c74a76b01ab485e80ffae07123
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-05-16 13:49:43 +0200

    Solve button

---
+++ src/components/Btn.vue
@@ -0,0 +1,7 @@
+<template>
+  <button class="bg-slate-200 p-4 w-full rounded-md hover:bg-slate-300">
+    <span class="text-4xl font-light">
+      <slot />
+    </span>
+  </button>
+</template>
---
+++ src/components/SolveBtn.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Btn from './Btn.vue';
+import { Sudoku } from '@/helpers/sudoku';
+import { useGridStore } from '@/stores/grid';
+
+function solve(): void {
+  const store = useGridStore();
+  const sudoku = new Sudoku(store.grid);
+
+  sudoku.solve();
+}
+</script>
+
+<template>
+  <Btn @click="solve">Solve</Btn>
+</template>
