f876439551dbd4aea2498d62be922650f4559c90
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-05-16 12:13:00 +0200

    Attempt to fix #1

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -7,6 +7,19 @@ const gridSize = 9;
 const store = useGridStore();
 const { grid, selected } = storeToRefs(store);
 
+// Set default grid for testing
+store.grid = [
+  [9, 0, 3, 0, 0, 8, 4, 0, 0],
+  [0, 5, 0, 1, 0, 7, 0, 0, 3],
+  [6, 8, 7, 3, 0, 2, 9, 0, 0],
+  [0, 0, 0, 6, 0, 0, 0, 0, 0],
+  [5, 3, 8, 2, 0, 0, 0, 0, 0],
+  [1, 0, 2, 0, 0, 4, 0, 9, 0],
+  [3, 0, 0, 9, 0, 6, 0, 7, 0],
+  [7, 2, 6, 8, 5, 1, 0, 4, 0],
+  [0, 0, 0, 4, 0, 3, 2, 5, 0],
+];
+
 /**
  * Updates the selected property of the store with the provided row and column values.
  *
@@ -22,18 +35,12 @@ function select(row: number, col: number): void {
 <template>
   <table class="border-collapse">
     <tr v-for="(row, rowIndex) in gridSize" :key="row" class="bg-slate-200 text-center">
-      <td @click="select(rowIndex, columnIndex)" v-for="(column, columnIndex) in gridSize" :key="column" :class="[
-        'p-2',
-        'border',
-        'border-gray-300',
-        'w-12',
-        'h-12',
-        'cursor-pointer',
-        'hover:bg-slate-300',
-        `bg-slate-${(selected[0] === rowIndex && selected[1] == columnIndex) ? '400' : '200'}`,
-        (row % 3 === 0 && row !== gridSize) ? 'border-b-4' : '',
-        (column % 3 === 0 && column !== gridSize) ? 'border-r-4' : '',
-      ].join(' ')">
+      <td @click="select(rowIndex, columnIndex)" v-for="(column, columnIndex) in gridSize" :key="column"
+        class="p-2 border border-gray-300 w-12 h-12 cursor-pointer hover:bg-slate-300" :class="[
+          `bg-slate-${(selected[0] === rowIndex && selected[1] == columnIndex) ? '400' : '200'}`,
+          (row % 3 === 0 && row !== gridSize) ? 'border-b-4' : '',
+          (column % 3 === 0 && column !== gridSize) ? 'border-r-4' : '',
+        ].join(' ')">
         <span class="text-2xl font-light">
           <!-- Check if the value at the current grid position is 0, if not, render the value -->
           {{ grid[rowIndex][columnIndex] === 0
