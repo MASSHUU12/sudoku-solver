663a19dac6053e70172367928e4dc401c4324183
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-05-16 16:27:56 +0200

    Disabling button when solving Sudoku

--- src/components/Btn.vue
+++ src/components/Btn.vue
@@ -1,5 +1,11 @@
+<script setup lang="ts">
+const props = defineProps({
+  disabled: Boolean
+});
+</script>
+
 <template>
-  <button class="bg-slate-200 p-4 w-full rounded-md hover:bg-slate-300">
+  <button :disabled="props.disabled" class="bg-slate-200 p-4 w-full rounded-md hover:bg-slate-300 disabled:bg-slate-400">
     <span class="text-4xl font-light">
       <slot />
     </span>
--- src/components/SolveBtn.vue
+++ src/components/SolveBtn.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import Btn from './Btn.vue';
 import { Sudoku } from '@/helpers/sudoku';
 import { useGridStore } from '@/stores/grid';
 
+const solving = ref(false);
+
 function solve(): void {
   const store = useGridStore();
+
+  // Disable button when solving starts
+  solving.value = true;
   const sudoku = new Sudoku(store.grid);
+  solving.value = false; // Enable button when solving is finished
 
   // Display solved Sudoku
   store.$state.grid = sudoku.solve();
@@ -13,5 +20,5 @@ function solve(): void {
 </script>
 
 <template>
-  <Btn @click="solve">Solve</Btn>
+  <Btn @click="solve" :disabled="solving">Solve</Btn>
 </template>
