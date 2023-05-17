1b0a594fd3b94e38c52d1b63dda4ce7a27f12407
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-05-17 14:30:53 +0200

    Status translated

--- src/components/Status.vue
+++ src/components/Status.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
+import { useI18n } from "vue-i18n";
 import { useGridStore } from '@/stores/grid';
+
+const { t } = useI18n();
 
 const store = useGridStore();
 </script>
 
 <template>
-  <span v-if="store.timeFinished === 0" class="text-slate-700 font-semibold">Click Solve button, and we will solve Sudoku
-    for you!</span>
-  <span v-if="store.timeFinished > 0" class="text-green-700 font-semibold">Sudoku solved! Time: {{ (store.timeFinished /
-    1000).toFixed(2) }}s</span>
-  <span v-if="store.timeFinished <= -1" class="text-red-700 font-semibold">Oops. It's impossible to solve this
-    Sudoku!</span>
+  <span v-if="store.timeFinished === 0" class="text-slate-700 font-semibold">{{ t("status.normal") }}</span>
+  <span v-if="store.timeFinished > 0" class="text-green-700 font-semibold">{{ t("status.solved", {
+    time:
+      (store.timeFinished /
+        1000).toFixed(2)
+  }) }}</span>
+  <span v-if="store.timeFinished <= -1" class="text-red-700 font-semibold">{{ t("status.invalid") }}</span>
 </template>
