2c5cdbce9c8c7ca401fa82ebe66037653dd93362
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-05-17 14:00:44 +0200

    i18n setup

---
+++ src/i18n/index.ts
@@ -0,0 +1,14 @@
+import { createI18n } from 'vue-i18n';
+import en from './locales/en.json';
+import pl from './locales/pl.json';
+
+export default createI18n({
+  locale: 'en',
+  fallbackLocale: 'pl',
+  globalInjection: true,
+  legacy: false,
+  messages: {
+    en,
+    pl
+  }
+});
