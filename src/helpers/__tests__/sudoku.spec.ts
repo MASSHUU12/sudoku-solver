6cff7f8c697163d25098289bc849f9df5b181725
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-05-22 16:45:08 +0200

    Testing isValidSudoku

--- src/helpers/__tests__/sudoku.spec.ts
+++ src/helpers/__tests__/sudoku.spec.ts
@@ -44,4 +44,38 @@ describe('Sudoku', () => {
       expect(result.grid).toEqual(grid);
     });
   });
+
+  describe('isValidSudoku', () => {
+    it('should return true for a valid Sudoku board', () => {
+      const sudoku = new Sudoku([
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+      ]);
+
+      expect(sudoku['isValidSudoku']()).toBe(true);
+    });
+
+    it('should return false for an invalid Sudoku board', () => {
+      const sudoku = new Sudoku([
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 7] // Invalid duplicate value
+      ]);
+
+      expect(sudoku['isValidSudoku']()).toBe(false);
+    });
+  });
 });
