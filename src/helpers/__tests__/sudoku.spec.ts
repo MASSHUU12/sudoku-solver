446600eb16affa651581c3e4d43ffefccab01993
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-05-22 16:45:31 +0200

    Testing backtracking

--- src/helpers/__tests__/sudoku.spec.ts
+++ src/helpers/__tests__/sudoku.spec.ts
@@ -78,4 +78,43 @@ describe('Sudoku', () => {
       expect(sudoku['isValidSudoku']()).toBe(false);
     });
   });
+
+  describe('backtracking', () => {
+    it('should solve a valid Sudoku board', () => {
+      const sudoku = new Sudoku([
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+      ]);
+      const result = sudoku['backtracking']();
+      expect(result).toBe(true);
+      // Verify that the solved grid is valid
+      expect(new Sudoku(sudoku['grid'])['isValidSudoku']()).toBe(true);
+    });
+
+    it('should return false for an invalid Sudoku board', () => {
+      const grid = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 7] // Invalid duplicate value
+      ];
+      const sudoku = new Sudoku(grid);
+      const result = sudoku['backtracking']();
+      expect(result).toBe(false);
+      // Verify that the grid remains unchanged
+      expect(sudoku['grid']).toEqual(grid);
+    });
+  });
 });
