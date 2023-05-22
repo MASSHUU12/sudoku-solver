95b6113971eede7febd8d34b4f69971bdfd70fcf
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-05-22 12:14:01 +0200

    Reset board when deselected templates

--- src/components/ExamplesSwitch.vue
+++ src/components/ExamplesSwitch.vue
@@ -24,17 +24,17 @@ function changeTemplate(e: Event): void {
   const selectedValue = (e.target as HTMLSelectElement).value;
   const selectedTemplate = templates as Templates;
 
-  if (selectedValue === "")
+  // If no template is selected, reset board
+  if (selectedValue === "") {
+    store.$reset();
     return;
+  }
 
   // Get the number of templates for the selected value
   const numOfTemplates = selectedTemplate[selectedValue].length;
 
   // Generate a random index for selecting a template
   const randomTemplateIndex = Math.floor(Math.random() * numOfTemplates);
-
-  // Reset grid to the initial state
-  store.$reset();
 
   // Set the selected template in the grid store
   store.$state.grid = selectedTemplate[selectedValue][randomTemplateIndex];
