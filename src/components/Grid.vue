794b68c8e8eb25d778c4f46878d47497cd5e9643
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-05-24 11:45:46 +0200

    Selected cells should now work in production

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -13,7 +13,7 @@ const { grid, selected } = storeToRefs(store);
     <tr v-for="(row, rowIndex) in gridSize" :key="row" class="bg-slate-200 text-center">
       <td @click="store.select([rowIndex, columnIndex])" v-for="(column, columnIndex) in gridSize" :key="column"
         class="p-1 border border-gray-300 w-8 h-8 md:w-12 md:h-12 cursor-pointer hover:bg-slate-300" :class="[
-          `bg-slate-${(selected[0] === rowIndex && selected[1] === columnIndex) ? '400' : '200'}`,
+          `slate-${(selected[0] === rowIndex && selected[1] === columnIndex) ? '400' : '200'}`,
           (row % 3 === 0 && row !== gridSize) && 'border-b-4',
           (column % 3 === 0 && column !== gridSize) && 'border-r-4'
         ]">
